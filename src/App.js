dd4a375f164172fcaa28eaa8f5838c8dc2338395
AbdoWahba <abdo.tw20154@gmail.com>
2020-09-17 23:57:26 +0200

    updating the main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import client, { refreshAuthHeaders } from './api/client';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuthState, setProfile, setRedirection } from './store/actions';
+import store from './store/store';
+import { BrowserRouter, Router, Redirect, Route, Link } from 'react-router-dom';
+
+import MainHeader from './Components/MainHeader';
+import LoginPage from './Pages/LoginPage';
+import SignupPage from './Pages/SignupPage';
+import NewMessagePage from './Pages/NewMessagePage';
+import OutboxPage from './Pages/OutboxPage';
+import InboxPage from './Pages/InboxPage';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const redirectAvailable = useSelector((state) => state.redirectAvailable);
+
+  useEffect(() => {
+    //Get Current User Profile (IF AVAILABLE)
+    client
+      .get('/users/profile/')
+      .then((response) => {
+        console.log(response.data);
+        dispatch(setProfile(response.data.user));
+      })
+      .catch((error) => {
+        console.log(error.response.data.msg);
+      });
+  }, []);
+
+  let redirectComponent = null;
+  if (redirectAvailable) {
+    redirectComponent = <Redirect to={redirectAvailable} />;
+    dispatch(setRedirection(null));
+  } else {
+    redirectComponent = null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <MainHeader />
+        {redirectComponent}
+        <Route exact path='/login' component={LoginPage} />
+        <Route exact path='/signup' component={SignupPage} />
+        <Route exact path='/new' component={NewMessagePage} />
+        <Route exact path='/outbox' component={OutboxPage} />
+        <Route exact path='/inbox' component={InboxPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
